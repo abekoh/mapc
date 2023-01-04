b7f8aa456eb0c5eeefe7d82692e5966e33e9c0e2
Kotaro Abe <abekoh.bcky@gmail.com>
2023-01-04 09:26:57 +0900

    update

--- internal/code/code.go
+++ internal/code/code.go
@@ -2,24 +2,38 @@ package code
 
 import (
 	"fmt"
+	"go/token"
+	"io"
 	"os"
 
 	"github.com/dave/dst"
 	"github.com/dave/dst/decorator"
+	"github.com/dave/dst/decorator/resolver/goast"
+	"github.com/dave/dst/decorator/resolver/guess"
 )
 
 type File struct {
-	file *dst.File
+	pkgPath string
+	dstFile *dst.File
 }
 
-func LoadFile(path string) (*File, error) {
-	f, err := os.ReadFile(path)
+func LoadFile(filePath, pkgPath string) (*File, error) {
+	f, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read file: %w", err)
 	}
-	df, err := decorator.Parse(f)
+	dec := decorator.NewDecoratorWithImports(token.NewFileSet(), pkgPath, goast.New())
+	df, err := dec.Parse(f)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse file: %w", err)
 	}
-	return &File{file: df}, nil
+	return &File{dstFile: df, pkgPath: pkgPath}, nil
+}
+
+func (f File) Write(w io.Writer) error {
+	r := decorator.NewRestorerWithImports(f.pkgPath, guess.New())
+	if err := r.Fprint(w, f.dstFile); err != nil {
+		return fmt.Errorf("failed to write: %w", err)
+	}
+	return nil
 }
