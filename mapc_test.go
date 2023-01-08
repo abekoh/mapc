293bb9321801471170e8bc46535fbb0beb7a2a1f
Kotaro Abe <abekoh.bcky@gmail.com>
2023-01-08 19:35:28 +0900

    tmp

--- mapc_test.go
+++ mapc_test.go
@@ -2,7 +2,6 @@ package mapc_test
 
 import (
 	"bytes"
-	"reflect"
 	"testing"
 	"time"
 
@@ -30,7 +29,7 @@ type BUser struct {
 
 func TestMapC(t *testing.T) {
 	m := mapc.New()
-	m.Register(AUser{}, BUser{})
+	m.Register(AUser{}, BUser{}, &mapc.Option{})
 	gs, errs := m.Generate()
 	for _, err := range errs {
 		require.Nil(t, err)
@@ -41,28 +40,5 @@ func TestMapC(t *testing.T) {
 		require.Nil(t, err)
 		assert.Equal(t, `
 `, buf.String())
-	}
-}
-
-type TypedInt int
-
-func TestTypeMapper(t *testing.T) {
-	a := 1
-	b := int64(2)
-	c := TypedInt(3)
-	d := "4"
-	e := mapc.TypedInt2(5)
-	rs := []reflect.Type{
-		reflect.TypeOf(a),
-		reflect.TypeOf(b),
-		reflect.TypeOf(c),
-		reflect.TypeOf(d),
-		reflect.TypeOf(e),
-	}
-	for _, x := range rs {
-		for _, y := range rs {
-			//t.Logf("assign %v -> %v: %v", x, y, x.AssignableTo(y))
-			t.Logf("convert %v -> %v: %v", x, y, x.ConvertibleTo(y))
-		}
 	}
 }
