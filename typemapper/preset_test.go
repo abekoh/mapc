88e5bde8f43fe64c6531bdafdf1eccac0d86fe0c
Kotaro Abe <abekoh.bcky@gmail.com>
2023-01-09 17:37:55 +0900

    refactor

--- typemapper/preset_test.go
+++ typemapper/preset_test.go
@@ -26,36 +26,13 @@ func TestAssignMapper_Map(t *testing.T) {
 		want   Caster
 		wantOk bool
 	}{
-		{
-			from:   Int,
-			to:     Int,
-			want:   &NopCaster{},
-			wantOk: true,
-		},
-		{
-			from:   String,
-			to:     String,
-			want:   &NopCaster{},
-			wantOk: true,
-		},
-		{
-			from:   Object,
-			to:     Object,
-			want:   &NopCaster{},
-			wantOk: true,
-		},
-		{
-			from:   Int,
-			to:     Int64,
-			want:   nil,
-			wantOk: false,
-		},
-		{
-			from:   Int,
-			to:     TypedInt,
-			want:   nil,
-			wantOk: false,
-		},
+		{from: Int, to: Int, want: &NopCaster{}, wantOk: true},
+		{from: String, to: String, want: &NopCaster{}, wantOk: true},
+		{from: Object, to: Object, want: &NopCaster{}, wantOk: true},
+		{from: Int, to: Int64, want: nil, wantOk: false},
+		{from: Int, to: TypedInt, want: nil, wantOk: false},
+		{from: Int, to: String, want: nil, wantOk: false},
+		{from: String, to: Int, want: nil, wantOk: false},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%s -> %s", tt.from, tt.to), func(t *testing.T) {
@@ -64,7 +41,88 @@ func TestAssignMapper_Map(t *testing.T) {
 				t.Errorf("Map() got = %v, want %v", got, tt.want)
 			}
 			if got1 != tt.wantOk {
-				t.Errorf("Map() gotOk = %v, want %v", got1, tt.wantOk)
+				t.Errorf("Map() gotOk = %v, wantOk %v", got1, tt.wantOk)
+			}
+		})
+	}
+}
+
+func TestConvertMapper_Map(t *testing.T) {
+	tests := []struct {
+		from   *types.Typ
+		to     *types.Typ
+		want   Caster
+		wantOk bool
+	}{
+		{
+			from: Int,
+			to:   Int,
+			want: &SimpleCaster{
+				pkgPath: "",
+				fn:      "int",
+			},
+			wantOk: true,
+		},
+		{
+			from: String,
+			to:   String,
+			want: &SimpleCaster{
+				pkgPath: "",
+				fn:      "string",
+			},
+			wantOk: true,
+		},
+		{
+			from: Object,
+			to:   Object,
+			want: &SimpleCaster{
+				pkgPath: "github.com/abekoh/mapc/testdata/sample",
+				fn:      "Object",
+			},
+			wantOk: true,
+		},
+		{
+			from: Int,
+			to:   Int64,
+			want: &SimpleCaster{
+				pkgPath: "",
+				fn:      "int64",
+			},
+			wantOk: true,
+		},
+		{
+			from: Int,
+			to:   TypedInt,
+			want: &SimpleCaster{
+				pkgPath: "github.com/abekoh/mapc/typemapper",
+				fn:      "typedInt",
+			},
+			wantOk: true,
+		},
+		{
+			from: Int,
+			to:   String,
+			want: &SimpleCaster{
+				pkgPath: "",
+				fn:      "string",
+			},
+			wantOk: true,
+		},
+		{
+			from:   String,
+			to:     Int,
+			want:   nil,
+			wantOk: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%s -> %s", tt.from, tt.to), func(t *testing.T) {
+			got, got1 := ConvertMapper{}.Map(tt.from, tt.to)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Map() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.wantOk {
+				t.Errorf("Map() gotOk = %v, wantOk %v", got1, tt.wantOk)
 			}
 		})
 	}
