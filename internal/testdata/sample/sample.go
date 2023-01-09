99a3a5af31fcc9617fe26214db813843d9553e49
Kotaro Abe <abekoh.bcky@gmail.com>
2023-01-09 16:27:00 +0900

    refactor

--- internal/testdata/sample/sample.go
+++ internal/testdata/sample/sample.go
@@ -6,6 +6,35 @@ import (
 	"github.com/google/uuid"
 )
 
+type Object struct {
+	Bool            bool
+	Int             int
+	Int8            int8
+	Int16           int16
+	Int32           int32
+	Int64           int64
+	Uint            uint
+	Uint8           uint8
+	Uint16          uint16
+	Uint32          uint32
+	Uint64          uint64
+	Uintptr         uintptr
+	Float32         float32
+	Float64         float64
+	Complex64       complex64
+	Complex128      complex128
+	IntArray        [5]int
+	IntChan         chan int
+	IntToStringFunc func(string) int
+	Interface       interface{}
+	StringIntMap    map[string]int
+	IntPointer      *int
+	Slice           []int
+	String          string
+	EmptyStruct     struct{}
+	// TODO: unsafe pointer
+}
+
 // AUser is source for mapping test
 type AUser struct {
 	ID           uuid.UUID
