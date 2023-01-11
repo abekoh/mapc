609383c018dfa2de2362fd6c310925618f784459
Kotaro Abe <abekoh.bcky@gmail.com>
2023-01-11 09:10:08 +0900

    test: add

---
+++ internal/mapping/mapping_test.go
@@ -0,0 +1,40 @@
+package mapping
+
+import (
+	"testing"
+
+	"github.com/abekoh/mapc/fieldmapper"
+	"github.com/abekoh/mapc/testdata/sample"
+	"github.com/abekoh/mapc/typemapper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMapper_NewMapping(t *testing.T) {
+	t.Run("map same field", func(t *testing.T) {
+		mapper := Mapper{
+			FieldMappers: fieldmapper.Default,
+			TypeMappers:  typemapper.Default,
+		}
+		got, err := mapper.NewMapping(sample.AUser{}, sample.BUser{})
+		require.Nil(t, err)
+		assert.Equal(t, "AUser", got.From.Name)
+		assert.Equal(t, "github.com/abekoh/mapc/testdata/sample", got.From.PkgPath)
+		assert.Equal(t, "BUser", got.To.Name)
+		assert.Equal(t, "github.com/abekoh/mapc/testdata/sample", got.To.PkgPath)
+		assert.Len(t, got.FieldPairs, 4)
+	})
+	t.Run("no fieldmappers,typemappers", func(t *testing.T) {
+		mapper := Mapper{
+			FieldMappers: []fieldmapper.FieldMapper{},
+			TypeMappers:  []typemapper.TypeMapper{},
+		}
+		got, err := mapper.NewMapping(sample.AUser{}, sample.BUser{})
+		require.Nil(t, err)
+		assert.Equal(t, "AUser", got.From.Name)
+		assert.Equal(t, "github.com/abekoh/mapc/testdata/sample", got.From.PkgPath)
+		assert.Equal(t, "BUser", got.To.Name)
+		assert.Equal(t, "github.com/abekoh/mapc/testdata/sample", got.To.PkgPath)
+		assert.Len(t, got.FieldPairs, 0)
+	})
+}
