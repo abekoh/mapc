cfaa24c340c85c74b8f4b7627802eda2588103a7
Kotaro Abe <abekoh.bcky@gmail.com>
2023-01-18 22:03:52 +0900

    test: add

--- e2e/types_test.go
+++ e2e/types_test.go
@@ -98,3 +98,48 @@ func ToSPointer(x S) SPointer {
 }
 `, got)
 }
+
+func Test_DeReference(t *testing.T) {
+	m := mapc.New()
+	m.Option.WithoutComment = true
+	m.Option.OutPkgPath = "github.com/abekoh/mapc/e2e/testdata/various"
+	m.Register(various.SPointer{}, various.S{})
+	gs, errs := m.Generate()
+	requireNoErrors(t, errs)
+	got, err := gs[0].Sprint()
+	require.Nil(t, err)
+	assert.Equal(t, `package various
+
+func ToS(x SPointer) S {
+	return S{
+		Bool:            *x.Bool,
+		Int:             *x.Int,
+		Int8:            *x.Int8,
+		Int16:           *x.Int16,
+		Int32:           *x.Int32,
+		Int64:           *x.Int64,
+		Uint:            *x.Uint,
+		Uint8:           *x.Uint8,
+		Uint16:          *x.Uint16,
+		Uint32:          *x.Uint32,
+		Uint64:          *x.Uint64,
+		Uintptr:         *x.Uintptr,
+		Float32:         *x.Float32,
+		Float64:         *x.Float64,
+		Complex64:       *x.Complex64,
+		Complex128:      *x.Complex128,
+		IntArray:        *x.IntArray,
+		IntChan:         *x.IntChan,
+		IntToStringFunc: *x.IntToStringFunc,
+		Interface:       x.Interface,
+		StringIntMap:    *x.StringIntMap,
+		IntPointer:      *x.IntPointer,
+		Slice:           *x.Slice,
+		String:          *x.String,
+		EmptyStruct:     *x.EmptyStruct,
+		ExternalType:    *x.ExternalType,
+		ExternalPointer: *x.ExternalPointer,
+	}
+}
+`, got)
+}
