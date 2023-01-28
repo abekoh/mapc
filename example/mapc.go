6125f0413c24d666458bc0fa0cc2448a957d1f21
Kotaro Abe <abekoh.bcky@gmail.com>
2023-01-28 17:29:29 +0900

    update

--- example/mapc.go
+++ example/mapc.go
@@ -5,13 +5,12 @@ import (
 
 	"github.com/abekoh/mapc"
 	"github.com/abekoh/mapc/example/domain"
+	"github.com/abekoh/mapc/example/graph/model"
 	"github.com/abekoh/mapc/example/infrastructure/sqlc"
 )
 
 func main() {
 	m := mapc.New()
-	m.Option.OutPath = "infrastructure/mapper.go"
-	m.Option.Mode = mapc.Deterministic
 
 	// TODO: set UserID like `type UserID uuid.UUID`, and setup custom caster
 	//m.Option.TypeMappers = append(m.Option.TypeMappers, mapcstd.TypeMapperFunc(
@@ -27,9 +26,22 @@ func main() {
 	//	}),
 	//)
 
-	m.Register(sqlc.User{}, domain.User{})
-	m.Register(sqlc.Task{}, domain.Task{})
-	m.Register(sqlc.SubTask{}, domain.SubTask{})
+	infra := m.Group(func(option *mapc.Option) {
+		option.OutPath = "infrastructure/mapper.go"
+		option.Mode = mapc.Deterministic
+	})
+
+	infra.Register(sqlc.User{}, domain.User{})
+	infra.Register(sqlc.Task{}, domain.Task{})
+	infra.Register(sqlc.SubTask{}, domain.SubTask{})
+
+	graph := m.Group(func(option *mapc.Option) {
+		option.OutPath = "graph/mapper.go"
+		option.Mode = mapc.Deterministic
+	})
+	graph.Register(sqlc.User{}, model.User{})
+	graph.Register(sqlc.Task{}, model.Task{})
+	graph.Register(sqlc.SubTask{}, model.SubTask{})
 
 	gs, errs := m.Generate()
 	if len(errs) > 0 {
