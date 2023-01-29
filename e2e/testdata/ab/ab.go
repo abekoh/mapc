5a4b6072ee7a24ab3673399a757b6285adca106e
Kotaro Abe <abekoh.bcky@gmail.com>
2023-01-29 21:08:46 +0900

    feat: add mapper

--- e2e/testdata/ab/ab.go
+++ e2e/testdata/ab/ab.go
@@ -19,3 +19,26 @@ type BUser struct {
 	Age          int
 	RegisteredAt time.Time
 }
+
+type AUser2 struct {
+	ID           string
+	Name         string
+	Age          int
+	RegisteredAt string
+}
+
+func MapStringToUUID(x string) (uuid.UUID, error) {
+	u, err := uuid.Parse(x)
+	if err != nil {
+		return [16]byte{}, err
+	}
+	return u, err
+}
+
+func MapStringToTime(x string) (time.Time, bool) {
+	t, err := time.Parse(x, time.RFC3339)
+	if err != nil {
+		return t, false
+	}
+	return t, true
+}
